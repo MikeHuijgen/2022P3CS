5f59489e3340fadfdf70f2953ea268a15795cc53
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-14 21:52:43 +0100

    Overerving(Inheritence) Bronze gemaakt

---
+++ Overerving(Inheritence) Bronze/Overerving(Inheritence) Bronze/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace Overerving_Inheritence__Bronze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.zoogDier();
+            Horse horse = new Horse();
+            horse.zoogDier();
+        }
+    }
+
+    class Animal
+    {
+        public void zoogDier()
+        {
+            Console.WriteLine("Dit een een zoogdier");
+        }
+    }
+
+    class Horse : Animal
+    {
+
+    }
+}
