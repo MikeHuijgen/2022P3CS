519236d64f80a650db221b507a1bfabbb0f63b46
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-14 21:32:39 +0100

    NameSpace Bronze gemaakt

---
+++ `NameSpace Bronze/`NameSpace Bronze/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _NameSpace_Bronze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HeyJij.SayHi text1 = new HeyJij.SayHi();
+            text1.sayHi();
+        }
+    }
+}
+
+namespace HeyJij
+{
+    class SayHi
+    {
+        public void sayHi()
+        {
+            Console.WriteLine("Hey vreemdeling, ik ben Mike");
+        }
+    }
+}
