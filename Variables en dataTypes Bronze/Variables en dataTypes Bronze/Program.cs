e28e0200b3819fb34a50d46dbe9f33dbca484f59
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-14 19:48:46 +0100

    Variables en datatypes Bronze gemaakt

---
+++ Variables en dataTypes Bronze/Variables en dataTypes Bronze/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables_en_dataTypes_Bronze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerNaam = "DeathSlayer";
+            int health = 100;
+            float damage = 24.8f;
+
+            Console.WriteLine(playerNaam);
+            Console.WriteLine(health);
+            Console.WriteLine(damage);
+        }
+    }
+}
