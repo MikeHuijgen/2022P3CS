ce0c2d3a17cb41ab9bbae8e78ac25e00323d01ea
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-14 20:01:49 +0100

    Loops Bronze gemaakt

---
+++ Loops Bronze/Loops Bronze/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Loops_Bronze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int tellen = 0; tellen < 101; tellen++)
+            {
+                Console.WriteLine(tellen);
+            }
+        }
+    }
+}
