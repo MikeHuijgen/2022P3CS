6c9d86958e1e5c25b8758d64c96d376ac81d7f2a
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-14 22:26:13 +0100

    Tijd op het scherm Bronze en Silver gemaakt Bronze gemaakt

---
+++ Tijd op het scherm Silver/Tijd op het scherm Silver/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tijd_op_het_scherm_Silver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            string datumTijd = "HH:mm:ss";
+            Console.WriteLine(time.ToString(datumTijd));
+        }
+    }
+}
---
+++ tijd op het scherm Bronze/tijd op het scherm Bronze/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Timers;
+
+namespace tijd_op_het_scherm_Bronze
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+            Timer tmer = new Timer(1000);
+            tmer.Elapsed += Tmer_Elapsed;
+            tmer.Enabled = true;
+            tmer.AutoReset = true;
+            tmer.Start();
+            
+            Console.ReadKey();         
+        }
+
+        private static void Tmer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            string datumTijd = "HH:mm:ss";
+            Console.WriteLine(time.ToString(datumTijd));
+        }
+    }
+}
