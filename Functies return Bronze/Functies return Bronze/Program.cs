9713a65a7c168a2b086f8077d0f4f072461c903d
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-14 19:43:00 +0100

    Functie return van brons en functies gemaakt

---
+++ Functies return Bronze/Functies return Bronze/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Functies_return_Bronze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(myName());
+        }
+
+        static string myName()
+        {
+            string naam = "Mike";
+            return naam;
+        }
+    }
+}
---
+++ Les1Functions/Les1Functions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Les1Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           HelloWorld();
+        }
+
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World! 2de keer");
+        }
+    }
+}
