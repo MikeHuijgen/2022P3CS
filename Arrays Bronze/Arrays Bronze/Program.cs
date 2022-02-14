cfb55cd62303e26f25ff9313b2ebed04e9f63a17
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-14 19:57:20 +0100

    Arrays Bronze gemaakt

---
+++ Arrays Bronze/Arrays Bronze/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Arrays_Bronze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 10, 20, 30, 40, 50, };
+
+            Console.WriteLine(myArray[2]);
+        }
+    }
+}
