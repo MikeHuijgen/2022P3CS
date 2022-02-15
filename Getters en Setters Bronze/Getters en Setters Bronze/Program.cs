e54bad42abbe5f2b11003fe4e9306484f58cdc19
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-15 18:06:51 +0100

    Getter en Setter Bronze gemaakt

---
+++ Getters en Setters Bronze/Getters en Setters Bronze/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Getters_en_Setters_Bronze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HealthPlayer healthPlayer = new HealthPlayer();
+            Console.WriteLine(healthPlayer.Health);
+        }
+    }
+
+    class HealthPlayer
+    {
+        private int healthPoints = 100;
+        
+        public HealthPlayer()
+        {
+            Health = healthPoints;
+        }
+
+        public int Health
+        {
+            get { return healthPoints; }
+            set { healthPoints = 150; }
+            
+        }
+           
+    }
+
+}
