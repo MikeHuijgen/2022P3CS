d8c8c6e7a68c1b6246d98ee7723d34283f8d4dfd
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-15 18:57:41 +0100

    Hello World Silver gemaakt

---
+++ Hello World Silver/Hello World Silver/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World_Silver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nHello World " + Console.ForegroundColor);
+        }
+    }
+}
