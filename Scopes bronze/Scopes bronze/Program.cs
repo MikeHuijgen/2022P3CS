7763e06cef8e016a5d36027b965b951c12808a47
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-15 18:44:19 +0100

    Scopes Bronze gemaakt

---
+++ Scopes bronze/Scopes bronze/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Scopes_bronze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 20;
+
+            Console.WriteLine(myAge);
+        }
+    }
+}
